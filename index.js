a60709f2af136272a5a9331ad7b928746084228e
Gifary Dhimas Fadhillah <gifarydhimas@gmail.com>
2018-01-17 20:44:36 +0700

    add logger update response structure update request handling update error handling update model update .gitignore

--- index.js
+++ index.js
@@ -1,44 +1,119 @@
 let express = require('express')
 let app = express()
 let bodyParser = require('body-parser')
+const logger = require('morgan')
 let Note = require('./models/Notes')
 
 app.use(bodyParser.json())
+// set the logger to watch every incoming connection
+app.use(logger('dev'))
 
 app.get('/', (req, res) => {
   Note.getAllNotes((err, data) => {
-    if (err) throw err
-    res.json(data)
+    if (err) {
+      res.status(500).json({
+        error: 'Internal Server Error',
+        message: 'Unable to fetch all notes'
+      })
+      console.error(err)
+      return
+    }
+    res.json({
+      notes: data
+    })
   })
 })
 
 app.post('/', (req, res) => {
   let note = req.body
+  if (!note.content || !note.title){
+    res.status(400).json({
+      error: "Bad Request",
+      message: "The content or the title is not defined"
+    })
+    return
+  }
+
   Note.addNote(note, err => {
-    if (err) throw err
-    res.json(
-      'Insert success with title: ' +
-        note.title +
-        ' and content: ' +
-        note.content
-    )
+    if (err){
+      res.status(500).json({
+        error: 'Internal Server Error',
+        message: 'Server is unable to insert new note'
+      })
+      console.error(err)
+      return
+    }
+    res.json({
+      message: 'Insert success with title: ' + note.title + ' and content: ' + note.content
+    })
   })
 })
 
 app.delete('/:id', (req, res) => {
   let id = req.params.id
   Note.deleteNote(id, err => {
-    if (err) throw err
-    res.json('Delete success with id: ' + id)
+    if (err) {
+      res.status(500).json({
+        error: 'Internal Server Error',
+        message: `Server is unable to delete note with id '${id}'`
+      })
+      console.error(err)
+      return
+    }
+    res.json({
+      message: 'Delete success with id: ' + id
+    })
   })
 })
 
 app.put('/:id', (req, res) => {
   let id = req.params.id
   let note = req.body
-  Note.updateNote(id, note, err => {
-    if (err) throw err
-    res.json('Update success with id: ' + id)
+
+  Note.getNoteById(id, (err, existingNotes) => {
+    const existingNote = existingNotes[0]
+    if (err){
+      res.status(500).json({
+        error: 'Internal Server Error',
+        message: 'Server is unable to fetch note with id: ' + id
+      })
+      console.error(err)
+      return
+    }
+    // first: make sure the record is existing
+    if (!existingNote) {
+      // tells the client if the note with id 'id' does not exist
+      res.status(404).json({
+        error: 'Not Found',
+        message: `Note with id '${id}' does not exist`
+      })
+      return
+    }
+   
+
+    // update note content if it is set in the request body
+    if (!note.content)
+      note.content = existingNote.content
+
+    // update note title if it is set in the request body
+    if (!note.title)
+      note.title = existingNote.title
+
+    Note.updateNote(id, note, err => {
+      if (err) {
+        res.status(500).json({
+          error:"Internal Server Error",
+          message: "Server is unable to update note"
+        })
+        console.error(err)
+        return
+      }
+
+      res.json({
+        message: 'Update success with id: ' + id
+      })
+
+    })
   })
 })
 
